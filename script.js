ae5030b31557c73209838c5e4dff398050397971
Daniel <danyulkoo@gmail.com>
2018-08-26 22:27:50 -0700

    Added timer buttons functionality as well as css options

--- script.js
+++ script.js
@@ -36,16 +36,54 @@ const timer = document.querySelector('#timer');
 
 let counter = 0;
 let timeLeft = 1500;
-timer.innerHTML = convertSeconds(timeLeft);
-
+let timerOn = false;
+timer.textContent = convertSeconds(timeLeft);
 
 function timeIt() {
 	if (counter < timeLeft)
 	{
 		counter++;
 	}
-	timer.innerHTML = convertSeconds(timeLeft - counter);
+	timer.textContent = convertSeconds(timeLeft - counter);
 }
 
+// Button functions
 var startBtn = document.querySelector('#start');
-startBtn.addEventListener('click', setInterval(timeIt,1000));
+var stopBtn = document.querySelector('#stop');
+var resetBtn = document.querySelector('#reset');
+
+var timerInterval;
+function startTimer() {
+	startBtn.classList.add("btnOn");
+	stopBtn.classList.remove("btnOn");
+	resetBtn.classList.remove("btnOn");
+	if (!timerOn)
+	{
+		timerInterval = setInterval(timeIt,1000);
+	}
+	timerOn = true;
+}
+
+function stopTimer() {
+	if (timerOn = true)
+	{
+		clearInterval(timerInterval)
+		stopBtn.classList.add("btnOn");
+		startBtn.classList.remove("btnOn");
+		resetBtn.classList.remove("btnOn");
+		timerOn = false;
+	}
+}
+
+function resetTimer() {
+	startBtn.classList.remove("btnOn");
+	stopBtn.classList.remove("btnOn");
+	counter = 0;
+	timeLeft = 1500;
+	timer.textContent = convertSeconds(timeLeft);
+	timerOn = false;
+}
+
+
+
+
