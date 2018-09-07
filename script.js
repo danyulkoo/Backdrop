e88f251841c5c1a2f73f591c861cc2648f8adf74
Daniel Koo <danyulkoo@gmail.com>
2018-09-06 21:45:19 -0700

    Updated script.js documentation

--- script.js
+++ script.js
@@ -5,38 +5,33 @@
 ///////////////////
 
 // converting seconds
-function convertSeconds(s) 
-{
+function convertSeconds(s) {
 	let min = Math.floor(s/60);
 	let seconds = s % 60;
 
-	if (min >= 10 && seconds < 10)
-	{
+	// Adding zeros to min and sec values less than 10
+	if (min >= 10 && seconds < 10) {
 		return min + ':0' + seconds;
 	}
-	else if (min < 10 && seconds >= 10)
-	{
+	else if (min < 10 && seconds >= 10) {
 		return '0' + min + ':' + seconds;
 	}
-	else if (min < 10 && seconds < 10)
-	{
+	else if (min < 10 && seconds < 10) {
 		return '0' + min + ':0' + seconds;
 	}
-	else if (min <= 0 && seconds <= 0)
-	{
+	else if (min <= 0 && seconds <= 0) {
 		return "00:00";
 	}
-	else
-	{
+	else {
 		return min + ':' + seconds;
 	}
 }
 
 // Timer variables
-const timer = document.querySelector('#timer');
-let studyTime = 1500;
-let brkTime = 300;
-var alarm = document.getElementById("alarm");
+const timer = document.querySelector('#timer'); // Timer HTML element 
+let studyTime = 1500; // 1500 seconds -> 25 min
+let brkTime = 300;	// 300 seconds -> 5 min
+var alarm = document.getElementById("alarm"); // Alarm HTML audio element
 
 let counter = 0;
 let timeLeft = studyTime;
@@ -55,9 +50,10 @@ function displayTimer() {
 }
 
 // Shuffle Button functions
-var shuffleBtn = document.querySelector('#shuffle');
-var title = document.querySelector('#pomodoro');
+var shuffleBtn = document.querySelector('#shuffle'); // Shuffle button HTML element
+var title = document.querySelector('#pomodoro'); // Title HTML element
 
+// Function to easily check if user is on work or break
 function checkPomodoro() {
 	if (title.textContent == "Work") {
 		return true;
@@ -74,18 +70,21 @@ var resetBtn = document.querySelector('#reset');
 
 var timerInterval;
 function startTimer() {
-	// Style buttons
+	// change buttons styles appropriately
 	startBtn.classList.add("btnOn");
 	stopBtn.classList.remove("btnOn");
 	resetBtn.classList.add("disabled");
+
+	// Turn timer on only if timer is not on already
 	if (!timerOn)
 	{
 		timerInterval = setInterval(timeIt,1000);
-		timerOn = true;
+		timerOn = true; // switch flag to show timer is on
 		timerReset = false;
 	}
 }
 
+// used later in the stopTimer() & resetTimer()
 function turnTimerOff() {
 	clearInterval(timerInterval);
 	timerOn = false;
@@ -106,9 +105,9 @@ function timeIt() {
 		alarm.play();
 
 		timerReset = true;
-		updateTimeSettings();
+		updateTimeSettings(); // this function written below under time settings section
 		
-		// Switch to Break when Timer runs out and vice versa
+		// Switch to Break if on Work and vice versa
 		if (checkPomodoro()) {
 			if (confirm("Switch to a short break?")) {
 				shuffle();
@@ -135,10 +134,13 @@ function timeIt() {
 	displayTimer();
 }
 
+// function to stop/pause timer
 function stopTimer() {
+	// only activate if timer is already running
 	if (timerOn)
 	{
 		turnTimerOff();
+		// change corresponding button styles
 		stopBtn.classList.add("btnOn");
 		startBtn.classList.remove("btnOn");
 		resetBtn.classList.remove("disabled");
@@ -147,10 +149,14 @@ function stopTimer() {
 	}
 }
 
+// resets timer completely
 function resetTimer() {
-	timerReset = true;
+	timerReset = true; // switch flag to show timer has been reset
+	
+	// only reset timer if timer is currently paused
+	// don't want to allow timer to be reset if its running
 	if (!timerOn) {
-		clearInterval(timerInterval)
+		turnTimerOff();
 		startBtn.classList.remove("btnOn");
 		stopBtn.classList.remove("btnOn");
 		counter = 0;
@@ -166,12 +172,16 @@ function resetTimer() {
 	}
 }
 
+// switches between work and break sessions
 function shuffle() {
+	// remove all button stylings
 	stopBtn.classList.remove("btnOn");
 	startBtn.classList.remove("btnOn");
 	resetBtn.classList.remove("disabled");
-	turnTimerOff();
+	turnTimerOff(); // turn timer off if it's running
 	updateTimeSettings();
+
+	// switch title to corresponding session
 	if (checkPomodoro()) {
 		title.textContent = "Break";
 		setTimer(brkTime);
@@ -183,32 +193,45 @@ function shuffle() {
 
 	displayTimer();
 }
+
 //////////////////
 // Sound navbar //
 //////////////////
 
 // function to toggle the sound
 function toggleSound(keyNum) {
-    const sound = document.querySelector(`audio[data-key="${keyNum}"]`);
-    const btn = document.querySelector(`button[data-key="${keyNum}"]`);
-    const dot = document.querySelector(`span[data-key="${keyNum}"]`)
-    if (!sound) return;
-
+	// select audio element corresponding to certain data key
+	const sound = document.querySelector(`audio[data-key="${keyNum}"]`);
+	// select button element corresponding to the same data key
+	const btn = document.querySelector(`button[data-key="${keyNum}"]`);
+	// select dot corresponding to the same data key
+	const dot = document.querySelector(`span[data-key="${keyNum}"]`)
+	
+	if (!sound) return; // return if there is no audio element
+	
+	// if sound is not playing
     if (sound.paused) {
-    	btn.classList.add('playing');
+		btn.classList.add('playing'); // add button style class
+		
+		// play sound
     	sound.currentTime = 0;
-    	sound.play();
+		sound.play();
+		
+		// show dot indicator
     	dot.classList.add('show');
-    }
+	}
+	// if sound is already playing, pause the sound
     else {
-    	btn.classList.remove('playing');
-    	sound.pause();
-    	dot.classList.remove('show');;
+    	btn.classList.remove('playing'); // remove button style class
+    	sound.pause(); // pause audio
+    	dot.classList.remove('show'); // hide dot indicator
     }
 }
 
 // get queryList of sound buttons
 const soundButtons = Array.from(document.querySelectorAll('.sound'));
+
+// iterate through the sound buttons
 soundButtons.forEach((button) => {
 	// When you click a sound button -> turn respective audio on
   	button.addEventListener('click', 
@@ -235,8 +258,10 @@ soundButtons.forEach((button) => {
   		});
 });
 
+// Get mute button element
 var muteBtn = document.getElementById('mute');
 
+// Hover effects
 muteBtn.onmouseover = function() {
 	if (!document.documentElement.classList.contains('is-touch')) {
 		muteBtn.children[0].style.opacity = "0.1";
@@ -250,8 +275,7 @@ muteBtn.onmouseout = function() {
 }
 
 muteBtn.onclick = function() {
-	// list of all audio elements
-	// pause all audio elements
+	// list of all audio elements and pause each sound
 	var sounds = document.getElementsByTagName('audio');
   	for(i = 0; i < sounds.length; i++) {
   		sounds[i].pause();
@@ -276,11 +300,13 @@ muteBtn.onclick = function() {
 let inputStudyTime = (document.getElementsByName("studytime")[0].value) * 60;
 let inputBrkTime = (document.getElementsByName('breaktime')[0].value)  * 60;
 
+// read the current time values in settings and store it into temp variables
 function readTimeSettings() {
 	inputStudyTime = (document.getElementsByName("studytime")[0].value) * 60;
 	inputBrkTime = (document.getElementsByName('breaktime')[0].value) * 60;
 }
 
+// update actual studyTime and brkTime variables
 function updateTimeSettings() {
 	studyTime = inputStudyTime;
 	brkTime = inputBrkTime;
@@ -303,7 +329,8 @@ var studyTimeBox = document.getElementsByName("studytime")[0];
 var brkTimeBox = document.getElementsByName('breaktime')[0];
 
 function formatSettingsInput () {
-	// format the time to look nice
+	// format the time in settings to look consistent
+	// i.e. "5 min" -> "05 min"
 	if (studyTimeBox.value < 10 && studyTimeBox.value[0] != "0") {
 		studyTimeBox.value = "0" + studyTimeBox.value;
 	}
@@ -330,9 +357,9 @@ savebtn.onclick = function () {
 	}
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x)
 span.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = "none"; // close modal
     studyTimeBox.value = inputStudyTime / 60;
 	brkTimeBox.value = inputBrkTime / 60;
 	formatSettingsInput();
@@ -342,7 +369,6 @@ span.onclick = function() {
 // Keyboard Inputs
 // space bar = start/stop (keycode = 32)
 // 'r' = reset (keycode = 82)
-
 window.addEventListener('keydown', function(e) {
 	if (e.keyCode == 83) {
 		if (timerOn) {
